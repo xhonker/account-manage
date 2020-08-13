17abab6a5584bef576ca1224edc38462f5cea49c
xhonker <wu_xhonker@hotmail.com>
2020-08-13 14:35:39 +0800

    chore:version

--- src/index.user.js
+++ src/index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         account-manage
 // @namespace    https://github.com/xhonker/account-manage
-// @version      0.1.14
+// @version      0.1.15
 // @description  internal account manage
 // @author       Quicker
 // @include      *://e.360.cn/
